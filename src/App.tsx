d56d9dd84cc6a9135f4bcf4c2a71028d7858eca1
Logassi <lomonagagaspas@gmail.com>
2024-09-26 21:05:30 +0700

    Feat : Creating ToDoListComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import TodoListComponent from './components/todolist'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='container'>
+      <h1>Chores ToDo List</h1>
+      <div id='todoList'>
+        <TodoListComponent/>
+        <TodoListComponent/>
+        <TodoListComponent/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      
+      <div id='contentAddTodo'>
+          <p>Done Counter : 0 / 0</p>
+          <div>
+            <p>Add ToDo</p>
+            <input/>
+            <button>Add Task</button>
+          </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/todolist.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function deleteButtonHandler(){
+    alert("Delete button pressed");
+}
+
+
+function TodoListComponent(){
+    return(
+        <div id='contentToDoList'>
+            <input type="checkbox"/> 
+            <label>Task</label>
+            <button onClick={deleteButtonHandler}>Delete Task</button>
+        </div>
+    )
+}
+
+export default TodoListComponent;
